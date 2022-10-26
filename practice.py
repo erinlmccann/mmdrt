d20241feb0bb987bb0b20830838707074ad511de
Erin McCann <erin.mccann@pnnl.gov>
2022-10-26 07:45:14 -0700

    update practice.py script

--- practice.py
+++ practice.py
@@ -1,5 +1,8 @@
 import os
+
 import pandas as pd
 
-inputPath = 'C:/Users/mcca512/OneDrive - PNNL/McCann Documents/Other Projects/SAR Analyses/'
-D = pd.read_csv(os.path.join(inputPath, 'returning_fish_unscreened.csv'))
+
+input_path = 'C:/Users/mcca512/OneDrive - PNNL/McCann Documents/Other Projects/SAR Analyses'
+
+df = pd.read_csv(os.path.join(input_path, 'returning_fish_unscreened.csv'))
