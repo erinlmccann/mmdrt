6a5760023085c9387103eaa44587807ccab79e31
Erin McCann <erin.mccann@pnnl.gov>
2022-11-28 14:31:30 -0800

    created copy of mm_sum_2022 for practice

--- practice.py
+++ practice.py
@@ -3,6 +3,6 @@ import os
 import pandas as pd
 
 
-input_path = 'C:/Users/mcca512/OneDrive - PNNL/McCann Documents/Other Projects/SAR Analyses'
+input_path = 'C:/Users/mcca512/OneDrive - PNNL/McCann Documents/MMD Adult Study/mmd_sum_2022/data/parquet_raw_data'
 
-df = pd.read_csv(os.path.join(input_path, 'returning_fish_unscreened.csv'))
+df = pd.read_parquet(os.path.join(input_path, '1_MITAS_20220626_20220627.parquet'), engine = 'pyarrow')
